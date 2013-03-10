34e65b5475b270a6504fdbea496869ff4bbdefb5
Endoscient <endoscient@me.com>
2013-03-10 15:27:30 -0400

    WebUI: Add context menu entries for starting walker.

--- updatorr/data/updatorr.js
+++ updatorr/data/updatorr.js
@@ -9,6 +9,16 @@ Deluge.plugins.UpdatorrPlugin = Ext.extend(Deluge.Plugin, {
 		false: _('Disable autoupdates')
 	},
 
+	runLabel: {
+		true: _('Check updates for selected'),
+		false: _('Torrent updates in progress...')
+	},
+
+	runAllLabel: {
+		true: _('Check for all updates'),
+		false: _('Torrent updates in progress...')
+	},
+
 	onEnable: function() {
 
 		this.tmSep = deluge.menus.torrent.add({
@@ -20,6 +30,18 @@ Deluge.plugins.UpdatorrPlugin = Ext.extend(Deluge.Plugin, {
 			handler: this.onToggleClick,
 			scope: this
 		});
+		
+		this.tmRun = deluge.menus.torrent.add({
+			text: this.runLabel[true],
+			handler: this.onRunClick,
+			scope: this
+		});
+		
+		this.tmRunAll = deluge.menus.torrent.add({
+			text: this.runAllLabel[true],
+			handler: this.onRunAllClick,
+			scope: this
+		});
 
 		deluge.menus.torrent.on('beforeshow', this.torrentMenuToShow, this);
 
@@ -28,8 +50,9 @@ Deluge.plugins.UpdatorrPlugin = Ext.extend(Deluge.Plugin, {
 
 	onDisable: function() {
 		deluge.menus.torrent.remove(this.tmSep);
-		deluge.menus.torrent.remove(this.tmAdd);
-		deluge.menus.torrent.remove(this.tmRemove);
+		deluge.menus.torrent.remove(this.tmToggle);
+		deluge.menus.torrent.remove(this.tmRun);
+		deluge.menus.torrent.remove(this.tmRunAll);
 		deluge.menus.torrent.un('beforeshow', this.torrentMenuToShow, this);
 		this.deregisterTorrentStatus('Updatorr');
 	},
@@ -39,19 +62,27 @@ Deluge.plugins.UpdatorrPlugin = Ext.extend(Deluge.Plugin, {
 		var balance = 0;
 
 		deluge.torrents.getSelections().forEach(function(entry) {
-			if (entry.data.Updatorr) { any = true; }
+			if (entry.data.Updatorr === "On") { any = true; }
 			balance += entry.data.Updatorr === "On" ? 1 : -1;
 		});
 
-		if (any) {
-			this.tmSep.show();
-			this.tmToggle.show();
-			this.tmToggle.setText(this.toggleLabel[balance < 0]);
-		}
-		else {
-			this.tmSep.hide();
-			this.tmToggle.hide();
-		}
+		this.tmToggle.setText(this.toggleLabel[balance < 0]);
+		this.tmRun.setVisible(any);
+		
+		deluge.client.updatorr.is_walking({success: function(is_walking) {
+			this.tmRun.setText(this.runLabel[!is_walking]);
+			this.tmRun.setDisabled(is_walking);
+			this.tmRunAll.setText(this.runAllLabel[!is_walking]);
+			this.tmRunAll.setDisabled(is_walking);
+		}, scope: this });
+	},
+
+	onRunClick: function(item) {
+		deluge.client.updatorr.run_walker( deluge.torrents.getSelectedIds() );
+	},
+
+	onRunAllClick: function(item) {
+		deluge.client.updatorr.run_walker(true);
 	},
 
 	onToggleClick: function(item) {
